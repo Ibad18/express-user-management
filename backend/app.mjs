0e3b926d21ccba6e1cf8b91b9b8f1ae16f8e6eed
Ibad18 <ibad1818@gmail.com>
2024-09-28 03:41:40 +0500

    created 'app.mjs' file in backend folder

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,19 +1,30 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
+app.use(express.json())
+app.use(cors())
 const users = [
     {id: 1, name: 'ibad', email:'ibad@gmail.com'},
     {id: 2, name: 'ibad2', email:'ibad2@gmail.com'},
     {id: 3, name: 'ibad3', email:'ibad3@gmail.com'}
 ]
+
+function errorHandler(err, req, res, next){
+    res.status(err.status || 500).json({
+        message: err.message || 'something went wrong',
+        error: true
+    })
+}
+
 app.get('/api/user', (req, res)=>{
     res.status(200).json({message:'fetching data', data: users})
 })
 app.post('/api/user', (req, res)=>{
-    const userId = users.length+1
+    const body = req.body;
+    // const newUserId = users.length+1
     const newUser = {
-        id: userId,
-        name: 'user'+userId,
-        email: 'abc'+userId+'@gmail.com'
+        id: users.length+1,
+        ...body
     }
     users.push(newUser)
     res.status(201).json({message:'fetching data', data: newUser})
