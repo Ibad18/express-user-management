241168607ffc5a861b5ec5ab1dbd55171e805612
Ibad18 <ibad1818@gmail.com>
2024-10-17 03:14:38 +0500

    deploying to vercel

--- backend/app.mjs
+++ backend/app.mjs
@@ -47,4 +47,4 @@ app.delete("/api/users/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app;
+// module.exports = app;
